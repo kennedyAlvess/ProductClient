bba04ca3ee0c606f8ee153e43ce810e4771c186b
kennedyAlvess <kennedy.pereira@alunos.ufersa.edu.br>
2025-02-03 23:20:07 -0300

    fix: ajustes campos client

--- ProductClient.Communication/RequestsDTO/RequestClient.cs
+++ ProductClient.Communication/RequestsDTO/RequestClient.cs
@@ -5,8 +5,6 @@ public class RequestClient
     public string Nome { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public DateOnly DataNascimento { get; set; }
-    public DateTime DataCadastro { get; set; }
-    public long UsuarioCadastro { get; set; }
     public string Cpf { get; set; } = string.Empty;
 
 }
