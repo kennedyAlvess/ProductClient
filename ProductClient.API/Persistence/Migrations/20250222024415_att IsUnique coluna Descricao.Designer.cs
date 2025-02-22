87038232610dd5f6e9fac849721962e550b5431a
kennedyAlvess <kennedy.pereira@alunos.ufersa.edu.br>
2025-02-21 23:51:59 -0300

    fix removendo restricao unique coluna descricao

--- ProductClient.API/Persistence/Migrations/20250222024415_att IsUnique coluna Descricao.Designer.cs
+++ ProductClient.API/Persistence/Migrations/20250222024415_att IsUnique coluna Descricao.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProductClient.API.Infrastructure;
 
@@ -11,9 +12,11 @@ using ProductClient.API.Infrastructure;
 namespace ProductClient.API.Persistence.Migrations
 {
     [DbContext(typeof(ProductClienteDbContext))]
-    partial class ProductClienteDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250222024415_att IsUnique coluna Descricao")]
+    partial class attIsUniquecolunaDescricao
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ProductClient.API/Persistence/Migrations/20250222024415_att IsUnique coluna Descricao.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductClient.API.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class attIsUniquecolunaDescricao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Products_Descricao",
+                table: "Products");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_Descricao",
+                table: "Products",
+                column: "Descricao");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Products_Descricao",
+                table: "Products");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_Descricao",
+                table: "Products",
+                column: "Descricao",
+                unique: true);
+        }
+    }
+}
