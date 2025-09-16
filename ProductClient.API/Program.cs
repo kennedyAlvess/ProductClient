cf901bbb4d37a1fffe53cbb949bbaa98eb25bbd9
kennedyAlvess <kenedy199717@gmail.com>
2025-09-16 20:04:52 -0300

    ref: ajustando para abrir navegador auto na pag correta

--- ProductClient.API/Program.cs
+++ ProductClient.API/Program.cs
@@ -1,6 +1,8 @@
 using ProductClient.API;
 using ProductClient.API.Filters;
 using ProductClient.API.Infrastructure.Configuration;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -33,8 +35,46 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// Redirecionar a rota raiz para o Swagger
+app.MapGet("/", () => Results.Redirect("/swagger"));
+
 app.UseCors();
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+
+// Abrir o navegador automaticamente em desenvolvimento
+if (app.Environment.IsDevelopment())
+{
+    var urls = app.Urls;
+    if (urls.Count > 0)
+    {
+        var url = urls.First().Replace("http://", "https://") + "/swagger";
+        OpenBrowser(url);
+    }
+}
+
 app.Run();
+
+static void OpenBrowser(string url)
+{
+    try
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            Process.Start("xdg-open", url);
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            Process.Start("open", url);
+        }
+    }
+    catch
+    {
+        // Se não conseguir abrir o navegador, não faz nada
+    }
+}
